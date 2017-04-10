beb518d1b8b3fab6df6675b21dc1405d5749f61e
DESKTOP-SPAH5K1\Alex <alexeistrug@gmail.com>
2017-04-10 15:37:25 +0300

    authenticate ver 2

--- autobase/services/src/main/java/com/sav/autobase/services/IAuthenticationService.java
+++ autobase/services/src/main/java/com/sav/autobase/services/IAuthenticationService.java
@@ -1,5 +1,7 @@
 package com.sav.autobase.services;
 
+import javax.naming.AuthenticationException;
+
 import com.sav.autobase.dao.impl.db.exceptions.DaoException;
 import com.sav.autobase.datamodel.Users;
 
@@ -7,6 +9,6 @@ public interface IAuthenticationService {
 	
 	boolean authenticate(String login, String password) throws DaoException;
 	
-	void create(Users newUser) throws DaoException;
+	void register(Users newUser) throws DaoException;
 
 }
--- autobase/services/src/main/java/com/sav/autobase/services/impl/AuthenticationServiceImpl.java
+++ autobase/services/src/main/java/com/sav/autobase/services/impl/AuthenticationServiceImpl.java
@@ -2,6 +2,8 @@ package com.sav.autobase.services.impl;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.sav.autobase.dao.impl.db.IUsersDao;
@@ -12,24 +14,26 @@ import com.sav.autobase.services.IAuthenticationService;
 @Service
 public class AuthenticationServiceImpl implements IAuthenticationService {
 
+	private final static Logger LOGGER = LoggerFactory.getLogger(AuthenticationServiceImpl.class);
+
 	@Inject
 	private IUsersDao usersDao;
 
 	@Override
 	public boolean authenticate(String UserLogin, String UserPassword) throws DaoException {
 		boolean flag = false;
-
 		Users authentication = usersDao.findByloginPassword(UserLogin, UserPassword);
-
 		if (authentication != null) {
+			LOGGER.info("Success authenticate User = " + UserLogin);
 			flag = true;
-		}
+		} else
+			LOGGER.info("Failed authenticate");
 		return flag;
 	}
 
 	@Override
-	public void create(Users newUsers) throws DaoException {
-
+	public void register(Users newUsers) throws DaoException {
+		LOGGER.info("Register new User");
 		usersDao.update(newUsers);
 	}
 
